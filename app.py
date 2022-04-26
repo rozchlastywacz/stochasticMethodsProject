b6042e2c0570a392dbaf136d780ef1a5340c38df
acwikla <arkadiusz.cwikla98@gmail.com>
2022-04-26 17:18:14 +0200

    sample multipage app

--- app.py
+++ app.py
@@ -1,30 +1,46 @@
-from dash import Dash, dcc, html
-from dash.dependencies import Input, Output
+import dash
+from dash import dcc, html, Output, Input, State
+import dash_labs as dl
+import dash_bootstrap_components as dbc
 
-from html_renderers import info_tab, turing_test_tab
+app = dash.Dash(
+    __name__, plugins=[dl.plugins.pages], external_stylesheets=[dbc.themes.BOOTSTRAP]
+)
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+offcanvas = html.Div(
+    [
+        dbc.Button("Explore", id="open-offcanvas", n_clicks=0),
+        dbc.Offcanvas(
+            dbc.ListGroup(
+                [
+                    dbc.ListGroupItem(page["name"], href=page["path"])
+                    for page in dash.page_registry.values()
+                    if page["module"] != "pages.not_found_404"
+                ]
+            ),
+            id="offcanvas",
+            is_open=False,
+        ),
+    ],
+    className="my-3"
+)
 
-app = Dash(__name__, external_stylesheets=external_stylesheets, title='stochasticMethodsProject')
-
-app.layout = html.Div([
-    html.H1('Generative models comparison', style={'textAlign': 'center'}),
-    dcc.Tabs(id="tabs-example-graph", value='tab-1-example-graph', children=[
-        dcc.Tab(label='About models', value='tab-1-example-graph'),
-        dcc.Tab(label='Turing test', value='tab-2-example-graph'),
-    ]),
-    html.Div(id='tabs-content-example-graph')
-])
+app.layout = dbc.Container(
+    [offcanvas, dl.plugins.page_container],
+    fluid=True,
+)
 
 
-@app.callback(Output('tabs-content-example-graph', 'children'),
-              Input('tabs-example-graph', 'value'))
-def render_content(tab):
-    if tab == 'tab-1-example-graph':
-        return info_tab.render()
-    elif tab == 'tab-2-example-graph':
-        return turing_test_tab.render()
+@app.callback(
+    Output("offcanvas", "is_open"),
+    Input("open-offcanvas", "n_clicks"),
+    [State("offcanvas", "is_open")],
+)
+def toggle_offcanvas(n1, is_open):
+    if n1:
+        return not is_open
+    return is_open
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
