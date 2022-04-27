fbc7787f5d73a6a58370792663d66b8103519eb4
acwikla <arkadiusz.cwikla98@gmail.com>
2022-04-27 08:51:46 +0200

    lighten requirements

--- models/dbn/test_dbn.py
+++ models/dbn/test_dbn.py
@@ -1,48 +1,48 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-import cv2
-import matplotlib.pyplot as plt
-import mnist
-import numpy as np
-
-from models.dbn.dbn import Dbn
-from models.utils import append_ones
-
-# digits = np.reshape(mnist.train_images()[:12*24], newshape=(12, 24, 28, 28))
-# tiles(digits)
-
-
-DATASET_SIZE = 20000  # 60000 for whole dataset
-DIGIT_SIZE = 28
-
-VISIBLE_LAYER_SIZE = DIGIT_SIZE * DIGIT_SIZE
-HIDDEN_LAYER_SIZE = 200
-
-mnist_train = mnist.train_images().astype(np.float32) / 255.0
-np.random.shuffle(mnist_train)
-dataset = np.reshape(mnist_train[:DATASET_SIZE], newshape=(DATASET_SIZE, DIGIT_SIZE * DIGIT_SIZE))
-dataset = append_ones(dataset)
-
-monitoring_indeces = np.random.choice(DATASET_SIZE, 256, replace=False)
-monitoring_set = dataset[monitoring_indeces]
-
-BATCH_SIZE = 128
-EPOCHS_COUNT = 50
-
-LEARNING_RATE = 0.1
-MOMENTUM = 0.5
-
-dbn = Dbn(VISIBLE_LAYER_SIZE, HIDDEN_LAYER_SIZE, LEARNING_RATE, MOMENTUM)
-dbn.load_weights()
-
-real_index = np.random.randint(0, DATASET_SIZE)
-real_image = mnist_train[real_index]
-generated_image = dbn.generate_image(real_image)
-
-real_image = cv2.resize(real_image, dsize=None, fx=4, fy=4)
-generated_image = cv2.resize(generated_image, dsize=None, fx=4, fy=4)
-
-fig, ax = plt.subplots(1, 2)
-ax[0].imshow(real_image, cmap='gray')
-ax[1].imshow(generated_image, cmap='gray')
-plt.show()
+# #!/usr/bin/python
+# # -*- coding: utf-8 -*-
+# import cv2
+# import matplotlib.pyplot as plt
+# import mnist
+# import numpy as np
+#
+# from models.dbn.dbn import Dbn
+# from models.utils import append_ones
+#
+# # digits = np.reshape(mnist.train_images()[:12*24], newshape=(12, 24, 28, 28))
+# # tiles(digits)
+#
+#
+# DATASET_SIZE = 20000  # 60000 for whole dataset
+# DIGIT_SIZE = 28
+#
+# VISIBLE_LAYER_SIZE = DIGIT_SIZE * DIGIT_SIZE
+# HIDDEN_LAYER_SIZE = 200
+#
+# mnist_train = mnist.train_images().astype(np.float32) / 255.0
+# np.random.shuffle(mnist_train)
+# dataset = np.reshape(mnist_train[:DATASET_SIZE], newshape=(DATASET_SIZE, DIGIT_SIZE * DIGIT_SIZE))
+# dataset = append_ones(dataset)
+#
+# monitoring_indeces = np.random.choice(DATASET_SIZE, 256, replace=False)
+# monitoring_set = dataset[monitoring_indeces]
+#
+# BATCH_SIZE = 128
+# EPOCHS_COUNT = 50
+#
+# LEARNING_RATE = 0.1
+# MOMENTUM = 0.5
+#
+# dbn = Dbn(VISIBLE_LAYER_SIZE, HIDDEN_LAYER_SIZE, LEARNING_RATE, MOMENTUM)
+# dbn.load_weights()
+#
+# real_index = np.random.randint(0, DATASET_SIZE)
+# real_image = mnist_train[real_index]
+# generated_image = dbn.generate_image(real_image)
+#
+# real_image = cv2.resize(real_image, dsize=None, fx=4, fy=4)
+# generated_image = cv2.resize(generated_image, dsize=None, fx=4, fy=4)
+#
+# fig, ax = plt.subplots(1, 2)
+# ax[0].imshow(real_image, cmap='gray')
+# ax[1].imshow(generated_image, cmap='gray')
+# plt.show()
--- models/utils.py
+++ models/utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 def append_ones(matrix, axis=1):
@@ -26,25 +26,25 @@ def chunks(seq, size):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
 
-def tiles(examples):
-    rows_count = examples.shape[0]
-    cols_count = examples.shape[1]
-    tile_height = examples.shape[2]
-    tile_width = examples.shape[3]
-
-    space_between_tiles = 2
-    img_matrix = np.empty(shape=(rows_count * (tile_height + space_between_tiles) - space_between_tiles,
-                                 cols_count * (tile_width + space_between_tiles) - space_between_tiles))
-    img_matrix.fill(np.nan)
-
-    for r in range(rows_count):
-        for c in range(cols_count):
-            x_0 = r * (tile_height + space_between_tiles)
-            y_0 = c * (tile_width + space_between_tiles)
-            ex_min = np.min(examples[r, c])
-            ex_max = np.max(examples[r, c])
-            img_matrix[x_0:x_0 + tile_height, y_0:y_0 + tile_width] = (examples[r, c] - ex_min) / (ex_max - ex_min)
-
-    plt.matshow(img_matrix, cmap='gray', interpolation='none')
-    plt.axis('off')
-    plt.show()
+# def tiles(examples):
+#     rows_count = examples.shape[0]
+#     cols_count = examples.shape[1]
+#     tile_height = examples.shape[2]
+#     tile_width = examples.shape[3]
+#
+#     space_between_tiles = 2
+#     img_matrix = np.empty(shape=(rows_count * (tile_height + space_between_tiles) - space_between_tiles,
+#                                  cols_count * (tile_width + space_between_tiles) - space_between_tiles))
+#     img_matrix.fill(np.nan)
+#
+#     for r in range(rows_count):
+#         for c in range(cols_count):
+#             x_0 = r * (tile_height + space_between_tiles)
+#             y_0 = c * (tile_width + space_between_tiles)
+#             ex_min = np.min(examples[r, c])
+#             ex_max = np.max(examples[r, c])
+#             img_matrix[x_0:x_0 + tile_height, y_0:y_0 + tile_width] = (examples[r, c] - ex_min) / (ex_max - ex_min)
+#
+#     plt.matshow(img_matrix, cmap='gray', interpolation='none')
+#     plt.axis('off')
+#     plt.show()
